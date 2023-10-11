5b94dec03ebb06c301c8b2125421d06377f9a339
DESKTOP-12J6D6C\Admin <huongvi1232003@gmail.com>
2023-10-11 22:18:06 +0700

    Init ex10

--- STM32Project/Core/Src/main.c
+++ STM32Project/Core/Src/main.c
@@ -61,6 +61,7 @@ static void MX_TIM2_Init(void);
 void updateClockBuffer(int hour, int min);
 void update7SEG(int index);
 void updateLEDMatrix(int index_led_matrix);
+void updateIndex();
 /* USER CODE END 0 */
 
 /**
@@ -79,10 +80,11 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-  //int index = 0;
-  //int counter = 0;
-  //int hour = 15, min = 8, sec = 50;
+  int index = 0;
+  int counter = 0;
+  int hour = 15, min = 8, sec = 50;
   int index_led_matrix = 0;
+  int counter_led_matrix = 0;
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -104,13 +106,43 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   setTimer1(25);
   //setTimer2(100);
-  //updateClockBuffer(hour, min);
+  setTimer3(50);
+  updateClockBuffer(hour, min);
   while (1)
   {
 	  if(timer1_flag == 1){
+		  if(counter > 3){
+			  sec++;
+			  if (sec >= 60){
+				  sec = 0;
+				  min++;
+			  }
+			  if(min >= 60){
+				  min = 0;
+				  hour++;
+			  }
+			  if(hour >=24){
+				  hour = 0;
+			  }
+			  updateClockBuffer(hour, min);
+			  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+			  counter = 0;
+		  }
+		  counter++;
+		  update7SEG(index++);
+		  if(index > 3) index = 0;
+		  setTimer1(25);
+	  }
+
+	  if(timer3_flag == 1){
+		  if(counter_led_matrix > 7){
+			  updateIndex();
+			  counter_led_matrix = 0;
+		  }
+		  counter_led_matrix++;
 		  updateLEDMatrix(index_led_matrix++);
 		  if(index_led_matrix > 7) index_led_matrix = 0;
-		  setTimer1(25);
+		  setTimer3(50);
 	  }
     /* USER CODE END WHILE */
 
@@ -300,7 +332,7 @@ void update7SEG(int index){
 }
 
 const int MAX_LED_MATRIX = 8;
-uint8_t matrix_buffer[8] = {0x18, 0x3c, 0x42, 0xc3, 0xff, 0xff, 0xc3, 0xc3};
+uint8_t matrix_buffer[8] = {0x18, 0x3c, 0x42, 0xc3, 0xff, 0xff, 0xc3, 0xc3};//A
 void updateLEDMatrix(int index){
     switch (index){
         case 0:
@@ -395,6 +427,19 @@ void updateLEDMatrix(int index){
             break;
     }
 }
+
+void updateIndex(){
+	uint8_t temp = matrix_buffer[0];
+	matrix_buffer[0] = matrix_buffer[1];
+	matrix_buffer[1] = matrix_buffer[2];
+	matrix_buffer[2] = matrix_buffer[3];
+	matrix_buffer[3] = matrix_buffer[4];
+	matrix_buffer[4] = matrix_buffer[5];
+	matrix_buffer[5] = matrix_buffer[6];
+	matrix_buffer[6] = matrix_buffer[7];
+	matrix_buffer[7] = temp;
+}
+
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 	timerRun();
 }
